1f3d6c89b5c514993e5f23666df422038cc476f6
jieming7 <jieming1907@gmail.com>
2025-08-04 10:42:33 +0800

    small changes

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Client {
+        public static void main(String[] args) {
+            ArrayList<Integer> a = new ArrayList();
+            a.add(10);
+            a.add(20);
+            a.add(30);
+            for  (int i=0; i<a.size(); i++) {
+                System.out.printf("%02d. " + a.get(i) + "%n", i+1);
+            }
+            System.out.println("removing index 1 (ie s/n 2)");
+            a.remove(1);
+            for  (int i=0; i<a.size(); i++) {
+                System.out.printf("%02d. " + a.get(i) + "%n", i+1);
+           }
+    }
 }
